208a0cc9c3c43a6cfaf1ad4c0cc0ed026c1af033
Uladzimir Ilchanka <yauhenij@gmail.com>
2025-02-13 13:49:09 +0300

    Подхорошил внешний вид интерфейса запроса типа данных

--- src/view/TypeSelector.java
+++ src/view/TypeSelector.java
@@ -9,7 +9,11 @@ public class TypeSelector {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            System.out.println("Выбрать тип данных для обработки. Доступные данные:" + "\n" + EntityType.BUS.name() + "\n" + EntityType.STUDENT.name() + "\n" + EntityType.USER.name() + "\n" + "Команда для выхода: " + Command.EXIT);
+            System.out.println("ВЫБЕРИТЕ ТИП ДАННЫХ, ВВЕДЯ СООТВЕТСТВУЮЩУЮ ЦИФРУ:" + "\n"
+                    + EntityType.BUS.getNumber() + ". " + EntityType.BUS.name() + "\n"
+                    + EntityType.STUDENT.getNumber() + ". " + EntityType.STUDENT.name() + "\n"
+                    + EntityType.USER.getNumber() + ". " + EntityType.USER.name() + "\n"
+                    + "Команда для выхода: " + Command.EXIT);
             String input = scanner.nextLine().toUpperCase();
 
             if (input.equals(Command.EXIT.name())) {
@@ -17,7 +21,12 @@ public class TypeSelector {
             }
 
             try {
-                return EntityType.valueOf(input);
+                String number = input.substring(0, 1);
+                for (EntityType type : EntityType.values()) {
+                    if (type.getNumber().equals(number)) {
+                        return type;
+                    }
+                }
             } catch (IllegalArgumentException e) {
                 System.out.println("Ошибка: неправильный ввод. Попробуйте еще раз. " + e.getMessage());
             }
